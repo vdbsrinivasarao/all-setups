6ac08795a55060d0c08e40603915e51ab01750d0
vdbsrinivasarao <113365336+vdbsrinivasarao@users.noreply.github.com>
2024-12-24 11:14:17 +0530

    Update 2023mysql.sh

--- 2023mysql.sh
+++ 2023mysql.sh
@@ -1,5 +1,11 @@
-sudo wget https://dev.mysql.com/get/mysql80-community-release-el9-1.noarch.rpm 
-sudo dnf install mysql80-community-release-el9-1.noarch.rpm -y
-sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2023
-sudo dnf install mysql-community-client -y
-sudo dnf install mysql-community-server -y
+wget https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm
+sudo yum localinstall mysql80-community-release-el7-3.noarch.rpm -y
+sudo yum repolist all | grep mysql
+sudo yum install mysql-community-client -y
+
+
+
+
+
+
+
